65bd45848acebc921e1a009a4cd40f3cfef30afa
speci <speci@LAPTOP-HUHMAAUK>
2021-01-20 09:40:37 -0800

    Initial

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Expression_bodied
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("whiteboard solutions container.");
+        }
+        }
+    }
+}
